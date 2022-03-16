6cba951788a5785e711a41e8e36ade60d200d03c
luisado123 <95661434+luisado123@users.noreply.github.com>
2022-03-16 12:37:17 -0500

    Add files via upload

---
+++ MyVet.Domain/Dto/ConsultarUsuariosDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyVet.Domain.Dto
+{
+    public class ConsultarUsuariosDto
+    {
+    
+
+       
+        public string Name { get; set; }
+
+      
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
